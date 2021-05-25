d8b63906794ed72e563c846e795c1416c9561a6f
Prady220 <pradeep.ece.gitam@gmail.com>
2021-05-25 01:09:46 -0500

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Address, User } from './app.model';
 import { UserService } from './app.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
-  public rawUsers = [];
   public users: User[] = [];
   public filteredUsers: User[] = [];
   searchName: String;
-  user: User;
-  address: Address;
 
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.getUsers();
+    this.getUsersFromService();
   }
 
-  getUsers() {
+  getUsersFromService() {
     this.userService.getUsers().subscribe(res => {
-      console.log(res.toString());
-      this.rawUsers = res as [];
-      this.rawUsers.forEach(u => {
-        this.users.push(this.convertToModel(u));
-      });
+      this.users = res;
       this.filteredUsers = this.users;
-      console.log(this.filteredUsers);
     });
-  }
-
-  convertToModel(user: User) {
-    this.address = new Address(
-      user.address.street,
-      user.address.suite,
-      user.address.city,
-      user.address.zipcode
-    );
-    this.user = new User(user.name, this.address);
-    return this.user;
   }
 
   filterUsers(searchTerm) {
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { User } from './app.model';
+import { Address, User } from './app.model';
+import { map } from 'rxjs/internal/operators/map';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,31 @@ export class UserService {
   private API_URL = 'https://jsonplaceholder.typicode.com/users';
   constructor(private http: HttpClient) {}
 
+  rawUsers = [];
+  users: User[] = [];
+  user: User;
+  address: Address;
+
   getUsers() {
-    return this.http.get<User[]>(this.API_URL);
+    return this.http.get(this.API_URL).pipe(
+      map(res => {
+        this.rawUsers = res as [];
+        this.rawUsers.forEach(u => {
+          this.users.push(this.convertToModel(u));
+        });
+        return this.users;
+      })
+    );
+  }
+
+  convertToModel(user: User) {
+    this.address = new Address(
+      user.address.street,
+      user.address.suite,
+      user.address.city,
+      user.address.zipcode
+    );
+    this.user = new User(user.name, this.address);
+    return this.user;
   }
 }
